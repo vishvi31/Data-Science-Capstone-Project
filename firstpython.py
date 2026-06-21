e1e77f643c9cc5bc9284be01f94dd95c258f9b0d
Vishvi <12thc.vishvi.30@gmail.com>
2026-06-21 17:44:46 +0530

    feat: add firstpython.py - my first file on GitHub

---
+++ firstpython.py
@@ -0,0 +1,9 @@
+# firstpython.py
+# Author: Vishvi
+
+print('Hello, World!')
+print('This is my first Python file on GitHub.')
+
+name = 'Vishvi'
+field = 'Data Science'
+print(name, 'is learning', field)
